aeef035f05c6196e09e1b2724cca50571607ec25
Alexander Eng <alexbird300@gmail.com>
2025-10-07 22:45:53 +0000

    added new things that are pretty cool

---
+++ 1_variables.js
@@ -0,0 +1,53 @@
+//one line comment
+
+
+/*
+here is multiple-line comments
+another comment here
+*/
+
+// 1------------ VAR ------------
+var oddNumber = 11;
+console.log(oddNumber);
+
+var evenNumber;
+evenNumber = 20;
+console.log(evenNumber);
+
+var specialVariable = null;
+console.log(specialVariable);
+
+//do not recommend
+//var num1 = 100, num2 = 101, num3 = 103;
+var num1 = 100;
+var num2 = 101;
+var num3 = 102;
+
+// variable redeclaration
+var number = 1000;
+
+var number = 1001;
+console.log(number);
+
+// variable reassignment
+var myNumber = 9;
+myNumber = 19;
+
+//---------- 2. LET --------
+let firstName = "Bronco";
+let lastName = "CPP";
+let anotherFullName = 'Full name is ' + firstName + " " + lastName;
+console.log(anotherFullName);
+
+let fullName = `Full name is ${firstName} ${lastName}`;
+console.log(fullName);
+
+let magicNumber;
+magicNumber = 13;
+
+// --------3. CONST ---------
+const PI_NUMBER = 3.14;
+
+
+
+
